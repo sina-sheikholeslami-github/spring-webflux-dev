3705c00d10819a185a14d5bee49d692b3a8a2288
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-07-30 21:35:46 -0700

    initial commit

---
+++ src/main/java/com/sinasheikholeslami/spring_webflux_dev/SpringWebfluxDevApplication.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.spring_webflux_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinasheikholeslami/spring_webflux_dev/SpringWebfluxDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.spring_webflux_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
